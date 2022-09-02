38af8ca34299784245ae4a16439b5763d73686d6
RJ Maganini <maganinirj@gmail.com>
2022-09-02 13:46:38 -0500

    adjust models to match updated schema

--- src-tauri/src/db/models.rs
+++ src-tauri/src/db/models.rs
@@ -1,39 +1,53 @@
-use super::schema::{question, question_topic};
+use super::schema::{question, question_topic, topic};
 
-#[derive(Queryable)]
-#[derive(QueryableByName)]
+#[derive(Queryable, QueryableByName)]
 #[table_name="question"]
 pub struct Question {
-    pub id: i32,
-    pub question_number: i32,
+    pub qid: i32,
     pub title: String,
-    pub title_slug: String,
-    pub prompt: String,
-    pub difficulty: String,
-    pub fetched: bool,
+    pub title_slug: Option<String>,
+    pub prompt: Option<String>,
+    pub difficulty: Option<String>,
+    pub source: Option<i32>,
+    pub source_qid: Option<i32>,
+}
+
+#[derive(Queryable, Clone)]
+pub struct Topic {
+    pub tid: i32,
+    pub name: String,
 }
 
 #[derive(Queryable)]
 pub struct QuestionTopic {
-    pub id: i32,
-    pub question_number: i32,
-    pub topic: String,
+    pub relid: i32,
+    pub qid: i32,
+    pub tid: i32,
 }
+
+
+
 
 #[derive(Insertable)]
 #[table_name="question"]
 pub struct NewQuestion {
-    pub question_number: i32,
     pub title: String,
     pub title_slug: String,
     pub prompt: String,
     pub difficulty: String,
-    pub fetched: bool
+    pub source: i32,
+    pub source_qid: i32,
+}
+
+#[derive(Insertable)]
+#[table_name="topic"]
+pub struct NewTopic {
+    pub name: String
 }
 
 #[derive(Insertable)]
 #[table_name="question_topic"]
 pub struct NewQuestionTopic {
-    pub question_number: i32,
-    pub topic: String,
+    pub qid: i32,
+    pub tid: i32,
 }
