4d46d9f2a0a6643a678c8d9147376b5c128efce1
RJ Maganini <maganinirj@gmail.com>
2022-09-03 20:50:45 -0500

    tauri command - preload leetcode questions into db

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -16,19 +16,44 @@ mod init;
 
 use db::lib::db_connect;
 use init::lib::get_lc_questions_on_init;
+use serde::Serialize;
+
+
+
+#[derive(Serialize)]
+struct CommandResult {
+    message: String,
+    status: i32
+}
+
+
+#[tauri::command]
+#[allow(dead_code)] 
+// rustc thinks this is dead, but its not. will be invoked as command from FE
+async fn preload_lc_questions_into_db() -> Result<CommandResult, CommandResult> {
+    let conn = db_connect();
+    let q_check = get_lc_questions_on_init(&conn).await;
+    let mut message: String = "Successfully preloaded leetcode questions".to_string();
+    let mut status: i32 = 200;
+    match q_check {
+        Ok(_) => {}
+        Err(err) => {
+            println!("leetcode question preload failed, {:?}", err);
+            message = "failed to preload leetcode questions".to_string();
+            status = 500;
+        }
+    }
+
+    Ok(CommandResult { message, status })
+}
+
+
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let conn = db_connect();
     diesel_migrations::run_pending_migrations(&conn).expect("migration error");
-
-    let q_check = get_lc_questions_on_init(&conn).await;
-    match q_check {
-        Ok(_) => {}
-        Err(err) => println!("error preloading leetcode questions: {:?}", err)
-    }
-    
 
     tauri::Builder::default()
         .run(tauri::generate_context!())
